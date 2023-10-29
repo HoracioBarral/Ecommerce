17c4eab87e8e54361442d599a812ca89931c16ae
HoracioBarral <horacio.barral@hotmail.com>
2023-10-29 17:18:31 -0300

    Primer push con modelo de clases y proyecto web

---
+++ dominio/Articulo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        int idArticulo { get; set; }
+
+        string nombreArticulo { set; get; }
+
+        Categoria categoria { get; set; }
+
+        Marca marca { get; set; }
+
+        decimal precio { get; set; }
+
+        int stock { get; set; }
+
+        bool Estado { get; set; }
+
+        List<Imagen> listaImagenes { get; set; }
+ 
+    }
+}
---
+++ dominio/Imagen.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Imagen
+    {
+        int idImagen { get; set; }
+
+        int idArticulo { get; set; }
+
+        string urlImagen { get; set; }
+    }
+}
