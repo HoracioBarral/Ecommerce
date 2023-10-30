ff3973ab8c0027b1f2089374aba5da213fab9b23
Robielys <116286724+Robielys@users.noreply.github.com>
2023-10-30 14:42:45 -0300

    Metodos de AccesoDatos funcionando
    set,abrir y cerrar conexion listas

--- Negocio/AccesoDatos.cs
+++ Negocio/AccesoDatos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,45 @@ namespace Negocio
 {
     public class AccesoDatos
     {
-        public void setConexion() { }
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
 
-        public void abrirConexion() { }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=Ecommerce; integrated security=true");
+            comando = new SqlCommand("");
+        }
+        public void setConexion(String consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
 
-        public void cerrarConexion() { }
+        public void abrirConexion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
     }
 }
