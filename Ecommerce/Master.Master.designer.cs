7faed2fb6df69d550a637fba2ee3f9a1153f29a7
Robielys <Robielys@users.noreply.github.com>
2023-11-14 16:33:04 -0300

    Barra de busqueda
    sin funcionalidad

--- Ecommerce/Master.Master.designer.cs
+++ Ecommerce/Master.Master.designer.cs
@@ -33,6 +33,15 @@ namespace Ecommerce
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Control txtSearch.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSearch;
+
+        /// <summary>
         /// Control LblVacio.
         /// </summary>
         /// <remarks>
