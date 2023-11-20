21f9c50200a788b149021ed9c028230352b0791b
Robielys <Robielys@users.noreply.github.com>
2023-11-20 14:57:32 -0300

    TryCatch para el pageload

--- Ecommerce/ModificarArticulo.aspx.cs
+++ Ecommerce/ModificarArticulo.aspx.cs
@@ -14,46 +14,55 @@ namespace Ecommerce
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ArticuloNegocio negocio = new ArticuloNegocio();
-            if(Session["usuario"] == null)
+            try
             {
-                Response.Redirect("Login.aspx", false);
+                ArticuloNegocio negocio = new ArticuloNegocio();
+                if (Session["usuario"] == null)
+                {
+                    Response.Redirect("Login.aspx", false);
+                }
+                if (!IsPostBack)
+                {
+                    if (Request.QueryString["nuevo"] == "true")
+                    {
+                        CategoriaNegocio cat = new CategoriaNegocio();
+                        List<Categoria> listaC = cat.listarCategorias();
+                        MarcaNegocio mar = new MarcaNegocio();
+                        List<Marca> listaM = mar.listarMarcas();
+                        ddlCategoria.DataSource = listaC;
+                        ddlCategoria.DataValueField = "idCategoria";
+                        ddlCategoria.DataTextField = "nombreCategoria";
+                        ddlCategoria.DataBind();
+                        ddlMarca.DataSource = listaM;
+                        ddlMarca.DataValueField = "idMarca";
+                        ddlMarca.DataTextField = "nombreMArca";
+                        ddlMarca.DataBind();
+                        // Lógica para cargar un nuevo artículo (puede ser limpiar los controles, por ejemplo)
+                        LimpiarControles();
+                    }
+                    else if (Request.QueryString["id"] != null)
+                    {
+                        int id = int.Parse(Request.QueryString["id"]);
+                        List<Articulo> lista = (List<Articulo>)(Session["listaArticulos"]);
+                        Articulo articulo = lista.Find(a => a.idArticulo == id);
+                        txtNombreArticulo.Text = articulo.nombreArticulo;
+                        txtDescripcion.Text = articulo.descripcion;
+                        ddlCategoria.Text = articulo.categoria.nombreCategoria;
+                        ddlMarca.Text = articulo.marca.nombreMarca;
+                        txtPrecio.Text = articulo.precio.ToString();
+                        txtStock.Text = articulo.stock.ToString();
+                        ImagenNegocio imagenNegocio = new ImagenNegocio();
+                        List<Imagen> imagenes = imagenNegocio.Listar(id);
+                        RepeaterImagenes.DataSource = imagenes;
+                        RepeaterImagenes.DataBind();
+                    }
+                }
             }
-            if (!IsPostBack)
+            catch (Exception ex)
             {
-                if (Request.QueryString["nuevo"] == "true")
-                {
-                    CategoriaNegocio cat = new CategoriaNegocio();
-                    List<Categoria> listaC = cat.listarCategorias();
-                    MarcaNegocio mar = new MarcaNegocio();
-                    List<Marca> listaM = mar.listarMarcas();
-                    ddlCategoria.DataSource = listaC;
-                    ddlCategoria.DataValueField = "idCategoria";
-                    ddlCategoria.DataTextField = "nombreCategoria";
-                    ddlCategoria.DataBind();
-                    ddlMarca.DataSource =listaM;
-                    ddlMarca.DataValueField = "idMarca";
-                    ddlMarca.DataTextField = "nombreMArca";
-                    ddlMarca.DataBind();
-                    // Lógica para cargar un nuevo artículo (puede ser limpiar los controles, por ejemplo)
-                    LimpiarControles();
-                }
-                else if (Request.QueryString["id"] != null )
-                {
-                    int id = int.Parse(Request.QueryString["id"]);
-                    List<Articulo> lista = (List<Articulo>)(Session["listaArticulos"]);
-                    Articulo articulo = lista.Find(a => a.idArticulo == id);
-                    txtNombreArticulo.Text = articulo.nombreArticulo;
-                    txtDescripcion.Text = articulo.descripcion;
-                    ddlCategoria.Text = articulo.categoria.nombreCategoria;
-                    ddlMarca.Text = articulo.marca.nombreMarca;
-                    txtPrecio.Text = articulo.precio.ToString();
-                    txtStock.Text = articulo.stock.ToString();
-                    ImagenNegocio imagenNegocio = new ImagenNegocio();
-                    List<Imagen> imagenes = imagenNegocio.Listar(id);
-                    RepeaterImagenes.DataSource = imagenes;
-                    RepeaterImagenes.DataBind();
-                }
+
+                Session.Add("Error", ex);
+                Response.Redirect("Error.aspx");
             }
         }
         private void LimpiarControles()
