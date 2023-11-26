15dd9fafac9e0278ec1bb9f3682827173d1723f0
HoracioBarral <horacio.barral@hotmail.com>
2023-11-26 20:53:13 -0300

    Se crea nueva Page ListadoPedidos2.aspx para administrar con mas eficiencia los pedidos

---
+++ Ecommerce/ListadoPedidos2.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="ListadoPedidos2.aspx.cs" Inherits="Ecommerce.ListadoPedidos2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server" EnablePartialRendering="true"></asp:ScriptManager>
+    <asp:UpdatePanel runat="server">
+        <ContentTemplate>
+            <div class="d-flex justify-content-between mb-3">
+                <h3>Listado de pedidos</h3>
+            </div>
+            <div class="d-flex justify-content-between mb-3">
+                <asp:Button runat="server" ID="btnVolver" Text="Volver" CssClass="btn btn-primary" OnClick="btnVolver_Click"/>
+            </div>
+            <asp:GridView ID="dgvPedidos" runat="server" AutoGenerateColumns="false" CssClass="table" DataKeyNames="idPedido" OnSelectedIndexChanged="dgvPedidos_SelectedIndexChanged" OnRowCommand="dgvPedidos_RowCommand">
+                <Columns>
+                    <asp:BoundField HeaderText="idPedido" DataField="idPedido" />
+                    <asp:BoundField HeaderText="ID Usuario" DataField="idUsuario" />
+                    <asp:BoundField HeaderText="Cantidad de Articulos" DataField="cantidad" />
+                    <asp:BoundField HeaderText="Importe" DataField="importe" />
+                    <asp:BoundField HeaderText="Estado" DataField="estado" />
+                    <asp:TemplateField HeaderText="Seleccione nuevo estado">
+                        <ItemTemplate>
+                            <asp:DropDownList runat="server" ID="ddlEstado" CssClass="form-control" AutoPostBack="true">
+                                <asp:ListItem Text="1 - En carrito" Value="1" />
+                                <asp:ListItem Text="2 - Compra Confirmada" Value="2" />
+                                <asp:ListItem Text="3 - Compra Abonada" Value="3" />
+                                <asp:ListItem Text="4 - Compra Cancelada" Value="4" />
+                                <asp:ListItem Text="5 - Entregado o Retirado" Value="5" />
+                            </asp:DropDownList>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:ButtonField ButtonType="Button" Text="Modificar Estado" CommandName="ModificarEstado" ControlStyle-CssClass="btn btn-link" />
+                    <asp:CommandField HeaderText="Ver Detalle" ShowSelectButton="true" SelectText="&#128077;" ControlStyle-CssClass="btn btn-link" />
+                </Columns>
+            </asp:GridView>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ Ecommerce/ListadoPedidos2.aspx.cs
@@ -0,0 +1,118 @@
+﻿using dominio;
+using Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ecommerce
+{
+    public partial class ListadoPedidos2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                recargarPedidos();
+            }
+        }
+
+        private void recargarPedidos()
+        {
+            PedidoNegocio pedidoNegocio = new PedidoNegocio();
+            List<Pedido> pedidos = new List<Pedido>();
+            pedidos = pedidoNegocio.listar();
+            dgvPedidos.DataSource = pedidos;
+            dgvPedidos.DataBind();
+        }
+
+        /*protected void ddlEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //GridViewRow row = dgvPedidos.SelectedRow;
+            //DropDownList ddlEstado = (DropDownList)row.FindControl("ddlEstado");
+            //int estado = int.Parse(ddlEstado.SelectedValue);
+            DropDownList ddlEstado = (DropDownList)sender;
+            int estado = int.Parse(ddlEstado.SelectedValue);
+            int idPedido = (int)(dgvPedidos.SelectedDataKey.Value);
+            PedidoNegocio pedidoNegocio = new PedidoNegocio();
+            List<Pedido> lista = pedidoNegocio.listar();
+            pedidoNegocio.actualizarEstado(estado, idPedido);
+            recargarPedidos();
+            if (estado == 4)
+            {
+                List<Pedido> pedido = lista.FindAll(x => x.idPedido == idPedido);
+                actualizarStock(idPedido);
+                if (pedido[0].estado != 1)
+                {
+                    UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+                    List<Usuario> usuarios = usuarioNegocio.Listar();
+                    List<Usuario> usuarioFiltrado = usuarios.FindAll(x => x.idUsuario == pedido[0].idUsuario);
+                    ServicioEmail mail = new ServicioEmail();
+                    mail.armarCorreo(usuarioFiltrado[0].nombreUsuario, "Compra Cancelada", "Su compra numero " + idPedido.ToString() + " fue cancelada");
+                    mail.enviarMail();
+                }
+            }
+        }*/
+
+        protected void dgvPedidos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Response.Redirect("DetallePedidos.aspx?id=" + dgvPedidos.SelectedValue, false);
+        }
+
+        protected void btnVolver_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Administrador2.aspx", false);
+        }
+
+        private void actualizarStock(int idPedido)
+        {
+            List<Articulo> articulosEliminados = new List<Articulo>();
+            PedidoNegocio pedidoNegocio = new PedidoNegocio();
+            articulosEliminados = pedidoNegocio.listarDetallePedido(idPedido);
+
+            foreach (Articulo articulo in articulosEliminados)
+            {
+                StockNegocio stockNegocio = new StockNegocio();
+                stockNegocio.modificarStock(articulo.idArticulo, articulo.talle, articulo.cantidad, true);
+            }
+        }
+
+        protected void dgvPedidos_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            //GridViewRow row = dgvPedidos.SelectedRow;
+            //DropDownList ddlEstado = (DropDownList)row.FindControl("ddlEstado");
+            //int estado = int.Parse(ddlEstado.SelectedValue);
+            if (e.CommandName != "ModificarEstado")
+            {
+                return;
+            }
+            int indice = Convert.ToInt32(e.CommandArgument);
+            GridViewRow selectedRow = dgvPedidos.Rows[indice];
+            DropDownList ddlEstado = (DropDownList)selectedRow.FindControl("ddlEstado");
+            DataGrid dgvPedido = (DataGrid)selectedRow.FindControl("dgvPedido");
+            int idPedido = (int)(dgvPedidos.DataKeys[selectedRow.RowIndex].Value);
+            int nuevoEstado = int.Parse(ddlEstado.SelectedValue);
+            PedidoNegocio pedidoNegocio = new PedidoNegocio();
+            List<Pedido> lista = pedidoNegocio.listar();
+            pedidoNegocio.actualizarEstado(nuevoEstado, idPedido);
+            recargarPedidos();
+            if (nuevoEstado == 4)
+            {
+                List<Pedido> pedido = lista.FindAll(x => x.idPedido == idPedido);
+                actualizarStock(idPedido);
+                if (pedido[0].estado != 1)
+                {
+                    UsuarioNegocio usuarioNegocio = new UsuarioNegocio();
+                    List<Usuario> usuarios = usuarioNegocio.Listar();
+                    List<Usuario> usuarioFiltrado = usuarios.FindAll(x => x.idUsuario == pedido[0].idUsuario);
+                    ServicioEmail mail = new ServicioEmail();
+                    mail.armarCorreo(usuarioFiltrado[0].nombreUsuario, "Compra Cancelada", "Su compra numero " + idPedido.ToString() + " fue cancelada");
+                    mail.enviarMail();
+                }
+            }
+        }
+    }
+}
